94fe837eb0467d545d3e14b45ebf39e136edf51a
596700 <596700@gmail.com>
2021-03-17 18:12:56 +0900

    マルチ認証

---
+++ backend/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// User
+Route::namespace('User')->prefix('user')->name('user.')->group(function () {
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset' => false,
+        'verify' => false,
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:user')->group(function () {
+
+        // トップページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+    });
+});
+
+// Administrator
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset' => false,
+        'verify' => false,
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:admin')->group(function () {
+
+        // トップページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+    });
+});
