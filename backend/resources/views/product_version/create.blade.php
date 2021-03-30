bd9b3daf3137eb53086299ba984d3122f65d70b0
596700 <596700@gmail.com>
2021-03-30 20:46:46 +0900

    view修正

--- backend/resources/views/product_version/create.blade.php
+++ backend/resources/views/product_version/create.blade.php
@@ -16,20 +16,24 @@
 
                     <p class="h4 mb-4 text-center">製品/バージョン登録</p>
 
-                    <select class="browser-default custom-select" id="select" name="product_id" required>
-                        <option value="" disabled selected>製品を選択してください</option>
-                        @foreach ($products as $product)
-                        <option value="{{ $product->id }}">{{ $product->name }}</option>
-                        @endforeach
-                    </select>
-                    
-                    <select class="browser-default custom-select" id="select" name="version_id" required>
-                        <option value="" disabled selected>バージョンを選択してください</option>
-                        @foreach ($versions as $version)
-                        <option value="{{ $version->id }}">{{ $version->version }}</option>
-                        @endforeach
-                    </select>
-                    
+                    <div class="form-group row">
+                        <select class="browser-default custom-select" id="select" name="product_id" required>
+                            <option value="" disabled selected>製品を選択してください</option>
+                            @foreach ($products as $product)
+                                <option value="{{ $product->id }}">{{ $product->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group row">
+                        <select class="browser-default custom-select" id="select" name="version_id" required>
+                            <option value="" disabled selected>バージョンを選択してください</option>
+                            @foreach ($versions as $version)
+                                <option value="{{ $version->id }}">{{ $version->version }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
                     <button class="btn btn-info btn-block" type="submit">Save</button>
                 </form>
             </div>
