9417fe81f6f022ae61aad4dda5e4b00f46b68df1
596700 <596700@gmail.com>
2021-04-23 13:00:28 +0900

    badgeの色変更

--- backend/resources/views/javascript/index_base_score.blade.php
+++ backend/resources/views/javascript/index_base_score.blade.php
@@ -6,15 +6,15 @@
         var score = baseScore[i].innerHTML;
 
         if (9.0 <= score && score <= 10.0) {
-            score += ' <span id="cvss_severity" class="badge badge-danger">Critical</span>';
+            score += ' <span id="cvss_severity" class="badge badge-dark">Critical</span>';
         } else if (7.0 <= score && score <= 8.9) {
-            score += ' <span id="cvss_severity" class="badge badge-warning">High</span>';
+            score += ' <span id="cvss_severity" class="badge badge-danger">High</span>';
         } else if (4.0 <= score && score <= 6.9) {
             score += ' <span id="cvss_severity" class="badge badge-warning">Medium</span>';
         } else if (0.1 <= score && score <= 3.9) {
-            score += ' <span id="cvss_severity" class="badge badge-warning">Low</span>';
+            score += ' <span id="cvss_severity" class="badge badge-secondary">Low</span>';
         } else {
-            score += ' <span id="cvss_severity" class="badge badge-dark">None</span>';
+            score += ' <span id="cvss_severity" class="badge badge-light">None</span>';
         }
         
         document.getElementsByClassName('base_score')[i].innerHTML = score;
--- backend/resources/views/javascript/show_base_score.blade.php
+++ backend/resources/views/javascript/show_base_score.blade.php
@@ -4,19 +4,19 @@
 
     if (9.0 <= baseScore.innerHTML && baseScore.innerHTML <= 10.0) {
         baseScore.innerHTML =
-            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-danger">Critical</span>';
+            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-dark">Critical</span>';
     } else if (7.0 <= baseScore.innerHTML && baseScore.innerHTML <= 8.9) {
         baseScore.innerHTML =
-            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-warning">High</span>';
+            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-danger">High</span>';
     } else if (4.0 <= baseScore.innerHTML && baseScore.innerHTML <= 6.9) {
         baseScore.innerHTML =
             '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-warning">Medium</span>';
     } else if (0.1 <= baseScore.innerHTML && baseScore.innerHTML <= 3.9) {
         baseScore.innerHTML =
-            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-warning">Low</span>';
+            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-secondary">Low</span>';
     } else {
         baseScore.innerHTML =
-            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-dark">None</span>';
+            '{{ $vulnerability->base_score }} <span id="cvss_severity" class="badge badge-light">None</span>';
     }
 
 </script>
