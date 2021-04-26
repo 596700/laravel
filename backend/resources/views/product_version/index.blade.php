4973d13c393fd458199187a152c731dd6b428803
596700 <596700@gmail.com>
2021-04-26 17:36:05 +0900

    レイアウト修正

--- backend/resources/views/product_version/index.blade.php
+++ backend/resources/views/product_version/index.blade.php
@@ -7,53 +7,49 @@
     @include('layouts.nav')
 
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-                @include('layouts.error_card_list')
+        @include('layouts.error_card_list')
 
-                <div class="d-flex justify-content-end">
-                    <form class="form-inline d-flex float-right md-form form-sm mt-0"
-                        action="{{ route('product_version.index') }}">
-                        <i class="fas fa-search" aria-hidden="true"></i>
-                        <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search"
-                            aria-label="Search" name="keyword" value="{{ empty(old()) ? $keyword : old('keyword') }}">
-                    </form>
-                </div>
-
-                <div class="d-flex justify-content-end">
-                    <a href="{{ route('product_version.create') }}"><button type="button"
-                            class="btn btn-outline-primary btn-sm">製品/バージョン登録</button></a>
-                </div>
-
-                @if ($product_versions->count())
-                    <div class="table-responsive-sm text-nowrap">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">製品/バージョン</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($product_versions as $product_version)
-                                    <tr>
-                                        <th scope="row">{{ $loop->iteration }}</th>
-                                        <td title="製品/バージョン詳細"><a
-                                                href="{{ route('product_version.show', ['product_version' => $product_version->id]) }}">
-                                                {{ $product_version->product->name }}/{{ $product_version->version->version }}</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            {{ $product_versions->appends(request()->input())->links() }}
-                        </div>
-                    </div>
-            </div>
+        <div class="d-flex justify-content-end">
+            <form class="form-inline d-flex float-right md-form form-sm mt-0"
+                action="{{ route('product_version.index') }}">
+                <i class="fas fa-search" aria-hidden="true"></i>
+                <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search"
+                    name="keyword" value="{{ empty(old()) ? $keyword : old('keyword') }}">
+            </form>
         </div>
+
+        <div class="d-flex justify-content-end">
+            <a href="{{ route('product_version.create') }}"><button type="button"
+                    class="btn btn-outline-primary btn-sm">製品/バージョン登録</button></a>
+        </div>
+
+        @if ($product_versions->count())
+            <div class="table-responsive-sm text-nowrap">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">製品/バージョン</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($product_versions as $product_version)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td title="製品/バージョン詳細"><a
+                                        href="{{ route('product_version.show', ['product_version' => $product_version->id]) }}">
+                                        {{ $product_version->product->name }}/{{ $product_version->version->version }}</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    {{ $product_versions->appends(request()->input())->links() }}
+                </div>
+            </div>
     </div>
 
 @else
--- backend/resources/views/version/index.blade.php
+++ backend/resources/views/version/index.blade.php
@@ -8,51 +8,45 @@
 
     <div class="container">
         @include('layouts.error_card_list')
-        <div class="row justify-content-center">
-            <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-                <div class="d-flex justify-content-end">
-                    <form class="form-inline d-flex float-right md-form form-sm mt-0"
-                        action="{{ route('version.index') }}">
-                        <i class="fas fa-search" aria-hidden="true"></i>
-                        <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search"
-                            aria-label="Search" name="keyword" value="{{ empty(old()) ? $keyword : old('keyword') }}">
-                    </form>
-                </div>
-
-                <div class="d-flex justify-content-end">
-                    <a href="{{ route('version.create') }}"><button type="button"
-                            class="btn btn-outline-primary btn-sm">バージョン登録</button></a>
-                </div>
-
-                @if ($versions->count())
-                    <div class="table-responsive-sm text-nowrap">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">バージョン名</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($versions as $version)
-                                    <tr>
-                                        <th scope="row">{{ $loop->iteration }}</th>
-                                        <td title="バージョン詳細"><a
-                                                href="{{ route('version.show', ['version' => $version->id]) }}">
-                                                {{ $version->version }}</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            {{ $versions->appends(request()->input())->links() }}
-                        </div>
-                    </div>
-            </div>
+        <div class="d-flex justify-content-end">
+            <form class="form-inline d-flex float-right md-form form-sm mt-0" action="{{ route('version.index') }}">
+                <i class="fas fa-search" aria-hidden="true"></i>
+                <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search"
+                    name="keyword" value="{{ empty(old()) ? $keyword : old('keyword') }}">
+            </form>
         </div>
+
+        <div class="d-flex justify-content-end">
+            <a href="{{ route('version.create') }}"><button type="button"
+                    class="btn btn-outline-primary btn-sm">バージョン登録</button></a>
+        </div>
+
+        @if ($versions->count())
+            <div class="table-responsive-sm text-nowrap">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">バージョン名</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($versions as $version)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td title="バージョン詳細"><a href="{{ route('version.show', ['version' => $version->id]) }}">
+                                        {{ $version->version }}</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    {{ $versions->appends(request()->input())->links() }}
+                </div>
+            </div>
     </div>
 
 @else
