36c401b9a1f01c4dec78dbd05d011fd8ac18f8e3
596700 <596700@gmail.com>
2021-04-29 20:37:36 +0900

    ajax対応

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
     {
         $this->authorizeResource(Product::class, 'product');
         $this->middleware('auth')->only('create');
-        // $this->middleware('verified')->only('create');
+        $this->middleware('verified')->only('create');
     }
 
     /**
