8d836754ebbbe2ef5c068d3553c079fa89543f36
596700 <596700@gmail.com>
2021-05-13 20:27:34 +0900

    usernameのmax_length修正

--- backend/app/Http/Requests/UpdateUser.php
+++ backend/app/Http/Requests/UpdateUser.php
@@ -24,7 +24,7 @@ class UpdateUser extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:30'],
             'email' => ['required', 'string', 'email', 'max:255'],
         ];
     }
