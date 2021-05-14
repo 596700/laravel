4651e71043c1e0134656778e54c8c94033983a76
596700 <596700@gmail.com>
2021-05-14 19:26:08 +0900

    Product修正フォームのレイアウト変更

--- backend/resources/views/product/edit.blade.php
+++ backend/resources/views/product/edit.blade.php
@@ -19,27 +19,50 @@
 
                     <p class="h4 mb-4 text-center">製品編集</p>
 
+                    <div class="form-group row">
+                        <input type="text" id="defaultUpdateFormProductName" class="form-control" placeholder="製品名"
+                            value="{{ $product->name }}" name="name">
+                        <small id="defaultUpdateFormProductNameHelpBlock" class="form-text text-muted">製品名は半角英数字記号スペース(-_
+                            )にしてください。</small>
+                        <div class="valid-feedback">適切な製品名の形式となっています。</div>
+                        <div class="invalid-feedback">製品名は半角英数字記号スペース(-_ )にしてください。</div>
+                    </div>
 
-                    <input type="text" id="defaultSaveFormProductName" class="form-control" placeholder="製品名"
-                        value="{{ $product->name }}" name="name">
+                    <div class="form-group row">
+                        <input type="text" id="defaultUpdateFormVendorURL" class="form-control" placeholder="ベンダURL"
+                            value="{{ $product->vendor_url }}" name="vendor_url">
+                        <small id="defaultUpdateFormVendorURLHelpBlock" class="form-text text-muted">
+                            ベンダURLはアクセス可能な正しいURLにしてください。<br />
+                            例(https://www.google.com/)
+                        </small>
+                        <div class="valid-feedback">
+                            適切なベンダURLの形式となっています。
+                        </div>
+                        <div class="invalid-feedback">
+                            ベンダURLはアクセス可能な正しいURLにしてください。<br />
+                            例(https://www.google.com/)
+                        </div>
+                    </div>
 
-                    <input type="text" id="defaultSaveFormVendorURL" class="form-control" placeholder="ベンダURL"
-                        value="{{ $product->vendor_url }}" name="vendor_url">
+                    <div class="form-group row">
+                        <select class="browser-default custom-select" id="defaultUpdateFormPart" name="part" required>
+                            <option value="{{ $product->part }}" selected>{{ $product->part }}</option>
+                            <option value="Hardware">Hardware</option>
+                            <option value="Operating System">Operating System</option>
+                            <option value="Application">Application</option>
+                        </select>
+                        <div class="valid-feedback">適切な選択となっています。</div>
+                        <div class="invalid-feedback">適切な選択肢を選んでください。</div>
+                    </div>
 
-                    <select class="browser-default custom-select" id="select" name="part" required>
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="Hardware">Hardware</option>
-                        <option value="Operating System">Operating System</option>
-                        <option value="Application">Application</option>
-                    </select>
 
-
-                    <button class="btn btn-info btn-block" type="submit">Save</button>
+                    <button class="btn btn-info btn-block" type="submit" id="defaultUpdateButton">Update</button>
 
                 </form>
 
             </div>
         </div>
     </div>
+@include('javascript.validate_update_product')
 
 @endsection
