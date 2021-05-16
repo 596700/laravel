688cea8f9888cdd968d72438ea66be7d3df1cac3
596700 <596700@gmail.com>
2021-05-16 11:15:37 +0900

    フラッシュメッセージレイアウト修正

--- backend/resources/views/layouts/flash_message.blade.php
+++ backend/resources/views/layouts/flash_message.blade.php
@@ -1,8 +1,10 @@
 @if (session('flash_message'))
-    <div class="card-text text-left success alert-success">
-        <ul class="m-3">
-            <!-- フラッシュメッセージ -->
-            <li>{{ session('flash_message') }}</li>
-        </ul>
+    <div class="card mx-auto">
+        <div class="card-text text-left success alert-success">
+            <ul class="m-3">
+                <!-- フラッシュメッセージ -->
+                <li>{{ session('flash_message') }}</li>
+            </ul>
+        </div>
     </div>
 @endif
